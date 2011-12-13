efb5608e191b95e6660504cf927808801c5d08c9
Sebastien Estienne <sebastien.estienne@gmail.com>
2011-12-13 22:59:27 +0100

    random display

--- wall.js
+++ wall.js
@@ -5,52 +5,60 @@ DM.init({
 });
 
 function init() {
-    var width = screen.width;
-    var height =  screen.height;
+    var width = $('body').width();
+    var height = $('body').height();
 
     var paper = Raphael(0, 0, width, height);
-
-    var pos_x = 0;
-    var pos_y = 0;
 
     DM.api('/videos', {limit: 100, fields: 'thumbnail_medium_url,title', filters: 'official', channel: 'music', sort: 'visited-week'}, function(response) {
         var list = response.list;
         var iter = 0;
 
-        function draw_elt() {
-            var thumb = paper.image(list[iter].thumbnail_medium_url, width/2, height/ 2, 160, 120);
-            thumb.toBack();
-            thumb.click(function() {
-                this.toFront().animate({x: width/2 - 320, y: height/2 - 240, width: 640, height: 480}, 500);
-            });
-
-            iter += 1;
-            if (iter  == response.list.length) {
-                return;
-            }
-            thumb.animate({x: pos_x, y: pos_y}, 75, 'linear', draw_elt);
+        var pos_x = 0;
+        var pos_y = 0;
+        var position = [];
+        for (var i = 0; i < response.list.length; i++) {
+            position.push({x: pos_x, y: pos_y});
             pos_x += 160;
             if (pos_x > width) {
                 pos_y += 120;
                 pos_x = 0;
             }
         }
-        draw_elt();
 
-        /*
-        response.list.forEach(function(element) {
-            //var thumb = paper.image(element.thumbnail_medium_url, pos_x, pos_y, 160, 120);
-            var thumb = paper.image(element.thumbnail_medium_url, 0, 0, 160, 120);
-            thumb.animate({x: pos_x, y: pos_y}, 1000, 'linear');
-            pos_x += 160;
-            if (pos_x == width) {
-                pos_y += 120;
-                pos_x = 0;
+        function draw_elt() {
+            rand_pos = Math.floor(Math.random() * position.length);
+            pos = position[rand_pos];
+            console.log(position.length);
+            position.splice(rand_pos, 1);
+            var thumb = paper.image(list[iter].thumbnail_medium_url, pos.x, pos.y, 160, 120);
+            thumb.toBack();
+            thumb.click(function() {
+                console.log(this.src);
+                this.toFront().animate({x: width/2 - 320, y: height/2 - 240, width: 640, height: 480}, 500);
+            });
+
+            thumb.hover(
+                function(){
+                    this.animate({opacity: 1, width: this.attrs.width+5, height: this.attrs.height+5}, 300);
+
+                },
+                function(){
+                    this.animate({opacity: 0.4, width: this.attrs.width-5, height: this.attrs.height-5}, 300);
+                })
+
+            iter += 1;
+            if (iter  == response.list.length) {
+                return;
             }
+            thumb.animate({opacity: 0.3}, 1000, '>');
+        }
 
-            console.log(pos_x);
-        });
-        */
+        for (var i = 0; i < response.list.length; i++) {
+            setTimeout(draw_elt, i * 20)
+        }
+
+        draw_elt();
     });
 }
 
